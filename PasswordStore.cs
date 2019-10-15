73cb2b4549aa16eae22ae2f2d0bdc6178e4c0b71
Alexander Kluth <clooze@protonmail.com>
2019-10-16 01:22:49 +0200

    Add project files.

---
+++ PasswordStore.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Tommy;
+
+namespace pass
+{
+    static class PasswordStore
+    {
+        private static readonly string passFileName = ".pass";
+
+        private static readonly string passFile =
+            Environment.GetEnvironmentVariable("homedrive")
+            + Environment.GetEnvironmentVariable("homepath")
+            + "\\"
+            + passFileName;
+
+        public static void Init()
+        {
+            if (!File.Exists(passFile))
+            {
+                File.WriteAllText(passFile, "[pass]");
+
+            }
+
+            else
+            {
+                File.Decrypt(passFile);
+            }
+        }
+
+        public static void DeInit()
+        {
+            File.Encrypt(passFile);
+        }
+
+        public static int Add(string title)
+        {
+            Console.WriteLine(title);
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            string password = "";
+            Console.Write("Password: ");
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key != ConsoleKey.Backspace && keyInfo.Key != ConsoleKey.Enter)
+                {
+                    password += keyInfo.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (keyInfo.Key == ConsoleKey.Backspace && password.Length > 0)
+                    {
+                        password = password.Substring(0, (password.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+
+            while (keyInfo.Key != ConsoleKey.Enter);
+
+            TomlTable newTableEntry = new TomlTable
+            {
+                [title] =
+                {
+                    ["username"] = username,
+                    ["password"] = password
+                }
+            };
+
+            using (StreamWriter sw = File.AppendText(passFile))
+            {
+                newTableEntry.ToTomlString(sw);
+            }
+
+            Console.WriteLine("\nAdded " + title + " to password store.");
+
+            return 0;
+        }
+
+        public static int Remove(string title)
+        {
+            TomlTable updatedTable = DeletePasswordEntryFromTable(title);
+            if (updatedTable == null) { return 1; }
+
+            using (StreamWriter sw = new StreamWriter(passFile))
+            {
+                updatedTable.ToTomlString(sw);
+            }
+
+            Console.WriteLine("Removed " + title + " from password store.");
+
+            return 0;
+        }
+
+        public static int Get(string title)
+        {
+            GetPasswordEntryFromTable(title);
+            return 0;
+        }
+
+        private static bool GetPasswordEntryFromTable(string title)
+        {
+            using (StreamReader reader = new StreamReader(File.OpenRead(passFile)))
+            {
+                TomlTable table = TOML.Parse(reader);
+
+                if (table.HasKey(title))
+                {
+                    table.TryGetNode(title, out TomlNode node);
+                    Clipboard.SetText(node["password"]);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static TomlTable DeletePasswordEntryFromTable(string title)
+        {
+            using (StreamReader reader = new StreamReader(File.OpenRead(passFile)))
+            {
+                TomlTable table = TOML.Parse(reader);
+
+                if (table.HasKey(title))
+                {
+                    table.Delete(title);
+                    return table;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using CommandLine;
+using System;
+
+namespace pass
+{
+    interface IOptions
+    {
+        [Value(0, MetaName = "Entry title",
+            HelpText = "Title of the new password entry",
+            Required = true)]
+        string Title { get; set; }
+    }
+
+    [Verb("add", HelpText = "Add new password to the store")]
+    class AddOptions : IOptions
+    {
+        public string Title { get; set; }
+    }
+
+    [Verb("remove", HelpText = "Remove a password from the store")]
+    class RemoveOptions : IOptions
+    {
+        public string Title { get; set; }
+    }
+
+    [Verb("get", HelpText = "Get a password from the store")]
+    class GetOptions : IOptions
+    {
+        public string Title { get; set; }
+    }
+
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            PasswordStore.Init();
+
+            int result = Parser.Default.ParseArguments<AddOptions, RemoveOptions, GetOptions>(args)
+                .MapResult(
+                  (AddOptions opts) => PasswordStore.Add(opts.Title),
+                  (RemoveOptions opts) => PasswordStore.Remove(opts.Title),
+                  (GetOptions opts) => PasswordStore.Get(opts.Title),
+                  errs => 1);
+
+            PasswordStore.DeInit();
+
+            return result;
+        }
+    }
+}
